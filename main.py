787b071bd1cc41014327fe5bbd3e47cace82e6ff
sashawhite@alex <alexandrkim.297@gmail.com>
2023-08-30 18:34:53 +0600

    decode to cp1251

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ password = "password"
 remote_path = "/<Partner code>/SELLTHR/"
 
 # Генерация данных для файлов INVOIC и INVRPT
-invoic_data = "G|GUID|...\r\nC|PartnerCode|...\r\nD|Data|..."
+invoice_data = "G|GUID|...\r\nC|PartnerCode|...\r\nD|Data|..."
 invrpt_data = "G|GUID|...\r\nS|Date1|Stats1|...\r\nS|Date2|Stats2|..."
 
-# Создание текстовых файлов
-with open("INVOIC.txt", "w", encoding="utf-8") as inv_file:
-    inv_file.write(invoic_data)
 
-with open("INVRPT.txt", "w", encoding="utf-8") as rpt_file:
+invoice_data = invoice_data.encode('utf-8').decode('cp1251', 'ignore')
+invrpt_data = invrpt_data.encode('utf-8').decode('cp1251', 'ignore')
+
+# Создание текстовых файлов
+with open("INVOIC.txt", "w", encoding="cp1251") as inv_file:
+    inv_file.write(invoice_data)
+
+with open("INVRPT.txt", "w", encoding="cp1251") as rpt_file:
     rpt_file.write(invrpt_data)
 
 # Упаковка в ZIP
